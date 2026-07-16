85b68c2c225197577db22f0aeac3e47545e32129
lusteroak <lusteroak@protonmail.com>
2026-07-16 11:35:03 -0500

    First Commit

---
+++ src/main/java/org/sinlicencia/stockifyapi/StockifyApiApplication.java
@@ -0,0 +1,13 @@
+package org.sinlicencia.stockifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockifyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sinlicencia/stockifyapi/StockifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sinlicencia.stockifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
